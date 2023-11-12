ca357bd2e877c953517199a9e37745c8e2f9a9b4
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2023-11-12 22:47:00 +0100

    OTHER (USv1 CM Consulte plan d'experimentation ) : Ajout du style pour la page et remise au propre du code (indentation et commentaires)

---
+++ sfapp/public/js/filter.js
@@ -0,0 +1,11 @@
+// Fonction pour ouvrir le popup
+function ouvrirPopupFiltreSalles() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Fonction pour fermer le popup
+function fermerPopupFiltreSalles() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
+}
