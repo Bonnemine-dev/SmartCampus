ce22085dd77bbb27e730d938e9e1cb8752aafc23
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2023-11-13 11:55:40 +0100

    FEAT (USv1 Consulter le plan d'expérimentation ) : Ajout d'un bouton réinitialiser pour le pop-up de filtre

--- sfapp/public/js/filter.js
+++ sfapp/public/js/filter.js
@@ -9,3 +9,20 @@ function fermerPopupFiltreSalles() {
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
 }
+
+document.getElementById('resetButton').addEventListener('click', function() {
+    // Ciblez toutes les cases à cocher et les boutons radio du formulaire
+    var checkboxes = document.querySelectorAll('.popup-filtre-salles input[type="checkbox"]');
+    var radios = document.querySelectorAll('.popup-filtre-salles input[type="radio"]');
+
+    // Décochez toutes les cases à cocher
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+
+    // Réinitialiser les boutons radio à une valeur par défaut si nécessaire
+    radios.forEach(function(radio) {
+        // Décochez ou réinitialisez les boutons radio ici si nécessaire
+        radio.checked = false;
+    });
+});
